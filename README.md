8e0aa4c213a1d823754377bbaf0fd3fc89fbbb33
nallouane <noahallouane@outlook.com>
2023-12-07 18:09:35 +0000

    modifies the milestone_3.py by adding the abstraction needed in milestone_2.py file. also updates README.md

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Hangman is a classic game in which a player thinks of a word and the other player tries to guess that word within a certain amount of attempts.
 
 This is an implementation of the Hangman game, where the computer thinks of a word and the user tries to guess it. 
+
+This project is implemented by using a while loop to first get a valid input letter. This input is then checked against
+a word to see if the letter is contained in the word. 
---
+++ milestone_3.py
@@ -0,0 +1,33 @@
+import random
+    
+list_of_favourite_fruit = [
+    "apple",
+    "banana",
+    "orange",
+    "pear",
+    "tomato"
+]
+
+print(list_of_favourite_fruit)
+
+random_word = random.choice(list_of_favourite_fruit)
+
+def ask_for_input():
+    while True:
+        single_letter_guess = input("Enter a single letter: ")
+        
+
+        if single_letter_guess.isalpha() and len(single_letter_guess) == 1:
+            print("Good guess!")
+            return single_letter_guess.lower()
+        else:
+            print("Oops! That is not a valid input. Please enter a single letter.")
+
+def check_guess(single_letter_guess, word):
+    if single_letter_guess in word:
+        print(f"Great! {single_letter_guess} is in the word.")
+
+    else:
+        print(f"Sorry, {single_letter_guess} is not in the word. Try again.")    
+                            
+check_guess(ask_for_input(), random_word)
