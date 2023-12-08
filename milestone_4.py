e1e9854af950bd5c2eab305bd72ec6e3f46688ad
nallouane <noahallouane@outlook.com>
2023-12-08 19:54:41 +0000

    Adds Class Hangman and defines the init magic method to file milestone_4.py

---
+++ milestone_4.py
@@ -0,0 +1,13 @@
+import random
+
+class Hangman:
+    def __init__(self, word_list, num_lives=5):
+        self.word_list = word_list
+        self.num_lives = num_lives
+        self.word = self._get_random_word()
+        self.word_guessed = ['_'] * len(self.word)
+        self.num_letters = len(set(self.word))
+        self.list_of_guesses = []
+
+    def _get_random_word(self):
+        return random.choice(self.word_list)
