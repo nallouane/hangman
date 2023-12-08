7fe3fae8ef1db24195232bd8b1b6659b179c2dcf
nallouane <noahallouane@outlook.com>
2023-12-08 20:56:24 +0000

    modifies the milestone_4.py by adding 'check' methods to validate input

--- milestone_4.py
+++ milestone_4.py
@@ -6,8 +6,38 @@ class Hangman:
         self.num_lives = num_lives
         self.word = self._get_random_word()
         self.word_guessed = ['_'] * len(self.word)
-        self.num_letters = len(set(self.word))
+        self.num_letters_left = len(set(self.word))
         self.list_of_guesses = []
 
     def _get_random_word(self):
         return random.choice(self.word_list)
+    
+    def check_guess(self, single_letter_guess):
+        if single_letter_guess in self.word:
+            print(f"Great! {single_letter_guess} is in the word.")
+
+        else:
+            print(f"Sorry, {single_letter_guess} is not in the word. Try again.")  
+    
+    def ask_for_input(self):
+        while True:
+            single_letter_guess = input("Enter a single letter: ")
+            
+            if not(single_letter_guess.isalpha() and len(single_letter_guess) == 1):
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif single_letter_guess in self.list_of_guesses:
+                print("Oops!You already tried that letter!")                    
+            else: 
+                self.check_guess(single_letter_guess)
+                self.list_of_guesses.append(single_letter_guess.lower())
+                
+list_of_favourite_fruit = [
+    "apple",
+    "banana",
+    "orange",
+    "pear",
+    "tomato"
+]                
+                
+person1 = Hangman(list_of_favourite_fruit)              
+person1.ask_for_input()    
