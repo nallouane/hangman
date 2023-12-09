f83760c0889ddbb993fd7aacb9b5a734dc99add1
nallouane <noahallouane@outlook.com>
2023-12-09 14:17:32 +0000

    renaming and optimising code

--- milestone_4.py
+++ milestone_4.py
@@ -1,9 +1,9 @@
 import random
 
 class Hangman:
-    def __init__(self, word_list, num_lives=5):
+    def __init__(self, word_list, num_lives_left=5):
         self.word_list = word_list
-        self.num_lives = num_lives
+        self.num_lives_left = num_lives_left
         self.word = self._get_random_word()
         self.word_guessed = ['_'] * len(self.word)
         self.num_letters_left = len(set(self.word))
@@ -19,12 +19,12 @@ class Hangman:
                 if single_letter_guess == letter:
                     self.word_guessed[index] = letter           
             self.num_letters_left -= 1        
-            print("You have {num_lives} lives left.")
+            print(f"You have {self.num_lives_left} lives left.")
 
         else:           
-            self.num_letters_left -= 1 
+            self.num_lives_left -= 1 
             print(f"Sorry, {single_letter_guess} is not in the word. Try again.")  
-            print("You have {num_lives} lives left.")
+            print(f"You have {self.num_lives_left} lives left.")
     
     def ask_for_input(self):
         while True:
